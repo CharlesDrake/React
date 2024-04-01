83f94aebbcdabe4e0e37738f7ca619f78c353192
Charles Drake <cdrake1205@gmail.com>
2024-04-01 19:00:42 -0400

    added components to the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
+import {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+const supabase = createClient('https://kncpxougvldubgtuqpyn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuY3B4b3VndmxkdWJndHVxcHluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NzIwNzcsImV4cCI6MjAyNjM0ODA3N30.r7Z0q1iWbLbmbOdhKKPgvZsT4JXstUKILfk1h0TMzX4')
+
+const isLoggedIn = true;
+
+function RandomName(){
+  const [name, setName] = useState('');
+  const names = ['Luna', 'Benjamin', 'Camila', 'Charles', 'Ava', 'Henry', 'Annalise', 'Alexander', 'Olivia', 'Elijah'];
+  const generate = () => { 
+    const name = 
+        names[Math.floor(Math.random() * names.length)]; 
+    setName(name); 
+  };
+  return ( 
+    <div className="namegenerator"> 
+        <h3> 
+            Random Name Generator 
+        </h3> 
+        <div className="button"> 
+            <button className="generate"
+                onClick={generate}> 
+                Name?
+            </button> 
+        </div> 
+        <p className="generated-name">  
+                {`${name}`} 
+            </p>
+    </div>
+  );
+}
+
+function GetBooks(){
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  async function getBooks(){
+    let { data } = await supabase.from('Books').select('*');
+    setBooks(data);
+  }
+
+  return (
+    <ul>
+      {books.map((book) => (
+        <li key={book.id}>{book.title} - {book.author}</li>
+      ))}
+    </ul>
+  );
+}
+
+function Button(){
+  function thing(){
+    alert('Thanks for logging in!!')
+  }
+  return(
+    <button onClick={thing}>Are You Logged In?</button>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi. My React project worked on locally!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Charles
-        </a>
+        <div>
+        {isLoggedIn ? (
+          <Button />
+        ) : (
+          <p>Please login.</p>
+        )}
+        </div>
+        <GetBooks />
+        <RandomName />
       </header>
     </div>
   );
